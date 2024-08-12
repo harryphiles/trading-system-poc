5571e22eec0e3a2f78ac9acf75fc43b11bd17e8d
harry@DESKTOP-V6L <l.bangsg@gmail.com>
2024-08-12 16:19:37 +0900

    Update MatchEngine codes due to introducing heap data structure in OrderQueue.

--- src/match_engine.py
+++ src/match_engine.py
@@ -1,4 +1,5 @@
-from src.order_components import OrderStatus
+from src.order_components import OrderStatus, Order
+from src.order_queue import OrderQueue
 from src.logger import Logger
 
 
@@ -6,53 +7,50 @@ class MatchEngine:
     def __init__(self, logger: Logger | None = None) -> None:
         self.logger = logger
 
-    def match_orders(
-        self, buy_orders, sell_orders, filled_orders
-    ) -> tuple[int, list[tuple]]:
+    def match_orders(self, order_queue: OrderQueue) -> tuple[int, list[tuple]]:
         matches = []
         num_removed_orders = 0
 
-        while buy_orders and sell_orders:
-            buy_high = max(buy_orders.keys())
-            sell_low = min(sell_orders.keys())
+        while True:
+            best_buy: Order = order_queue.get_best_buy_order()
+            best_sell: Order = order_queue.get_best_sell_order()
 
-            if buy_high >= sell_low:
-                buy_single_order = buy_orders[buy_high][0]
-                sell_single_order = sell_orders[sell_low][0]
-
-                matched_quantity = min(
-                    buy_single_order.quantity, sell_single_order.quantity
-                )
-                matches.append(
-                    (buy_single_order, sell_single_order, sell_low, matched_quantity)
-                )
-
-                buy_single_order.quantity -= matched_quantity
-                sell_single_order.quantity -= matched_quantity
-                buy_single_order.status = OrderStatus.PARTIALLY_FILLED
-                sell_single_order.status = OrderStatus.PARTIALLY_FILLED
-
-                if buy_single_order.quantity == 0:
-                    mark_filled = buy_orders[buy_high].pop(0)
-                    num_removed_orders += 1
-                    mark_filled.status = OrderStatus.FILLED
-                    filled_orders.append(mark_filled)
-                    if not buy_orders[buy_high]:
-                        del buy_orders[buy_high]
-
-                if sell_single_order.quantity == 0:
-                    mark_filled = sell_orders[sell_low].pop(0)
-                    num_removed_orders += 1
-                    mark_filled.status = OrderStatus.FILLED
-                    filled_orders.append(mark_filled)
-                    if not sell_orders[sell_low]:
-                        del sell_orders[sell_low]
-
-                self._log_matched_orders(
-                    buy_single_order, sell_single_order, matched_quantity, sell_low
-                )
-            else:
+            # Case for stopping matching process
+            if (
+                not best_buy or not best_sell or
+                best_sell.price > best_buy.price
+            ):
                 break
+
+            buy_order: Order = order_queue.remove_best_buy_order()
+            sell_order: Order = order_queue.remove_best_sell_order()
+
+            matched_quantity = min(buy_order.quantity, sell_order.quantity)
+            matches.append((buy_order, sell_order, sell_order.price, matched_quantity))
+
+            # Subtract quantity as result of transaction
+            buy_order.quantity -= matched_quantity
+            sell_order.quantity -= matched_quantity
+            # Mark each as partially_filled as preliminary action
+            buy_order.status = OrderStatus.PARTIALLY_FILLED
+            sell_order.status = OrderStatus.PARTIALLY_FILLED\
+
+            # Update Order according to its remaining quantity
+            if buy_order.quantity > 0:
+                order_queue.update_orderbooks(buy_order)
+            else:
+                num_removed_orders += 1
+                buy_order.status = OrderStatus.FILLED
+                order_queue.filled_orders.append(buy_order)
+
+            if sell_order.quantity > 0:
+                order_queue.update_orderbooks(sell_order)
+            else:
+                num_removed_orders += 1
+                sell_order.status = OrderStatus.FILLED
+                order_queue.filled_orders.append(sell_order)
+
+            self._log_matched_orders(buy_order, sell_order, matched_quantity, sell_order.price)
 
         return num_removed_orders, matches
 
