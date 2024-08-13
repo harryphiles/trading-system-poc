7c9dab11f06ece3b938a0601e843e10ef6220120
harry@DESKTOP-V6L <l.bangsg@gmail.com>
2024-08-13 20:52:45 +0900

    Update test cases for MatchEngine class.

--- tests/test_match_engine.py
+++ tests/test_match_engine.py
@@ -1,5 +1,6 @@
 import unittest
 from src.match_engine import MatchEngine
+from src.order_queue import OrderQueue
 from src.order_components import Order, OrderSide, OrderStatus
 from src.logger import Logger, LOGGING_CONFIG
 
@@ -8,88 +9,93 @@ class TestMatchEngine(unittest.TestCase):
     def setUp(self):
         self.logger = Logger(__name__, LOGGING_CONFIG, "test_match_engine.log").logger
         self.match_engine = MatchEngine(self.logger)
+        self.order_queue = OrderQueue()
         Order.reset_id_generator()
 
     def test_basic_match(self):
-        buy_orders = {100: [Order(1, OrderSide.BUY, 100, 10)]}
-        sell_orders = {100: [Order(2, OrderSide.SELL, 100, 10)]}
-        filled_orders = []
+        orders = [
+            Order(1, OrderSide.BUY, 100, 10),
+            Order(2, OrderSide.SELL, 100, 10),
+        ]
+        for order in orders:
+            self.order_queue.add_order(order)
+            self.order_queue.get_next_order()
 
-        num_removed, matches = self.match_engine.match_orders(
-            buy_orders, sell_orders, filled_orders
-        )
+        num_removed, matches = self.match_engine.match_orders(self.order_queue)
+        matched_buy_order, matched_sell_order, *_ = matches[0]
 
         self.assertEqual(num_removed, 2)
         self.assertEqual(len(matches), 1)
-        self.assertEqual(len(filled_orders), 2)
-        self.assertEqual(filled_orders[0].status, OrderStatus.FILLED)
-        self.assertEqual(filled_orders[1].status, OrderStatus.FILLED)
+        self.assertEqual(matched_buy_order.status, OrderStatus.FILLED)
+        self.assertEqual(matched_sell_order.status, OrderStatus.FILLED)
 
     def test_partial_match(self):
-        buy_orders = {100: [Order(1, OrderSide.BUY, 100, 15)]}
-        sell_orders = {100: [Order(2, OrderSide.SELL, 100, 10)]}
-        filled_orders = []
+        orders = [
+            Order(1, OrderSide.BUY, 100, 15),
+            Order(2, OrderSide.SELL, 100, 10),
+        ]
+        for order in orders:
+            self.order_queue.add_order(order)
+            self.order_queue.get_next_order()
 
-        num_removed, matches = self.match_engine.match_orders(
-            buy_orders, sell_orders, filled_orders
-        )
+        num_removed, matches = self.match_engine.match_orders(self.order_queue)
+        matched_buy_order, matched_sell_order, *_ = matches[0]
+
 
         self.assertEqual(num_removed, 1)
-        self.assertEqual(len(matches[0]), 4)
-        self.assertEqual(len(filled_orders), 1)
-        self.assertEqual(
-            filled_orders[0].order_id, "00000002"
-        )
-        self.assertEqual(buy_orders[100][0].quantity, 5)
-        self.assertEqual(buy_orders[100][0].status, OrderStatus.PARTIALLY_FILLED)
+        self.assertEqual(len(matches), 1)
+        self.assertEqual(matched_buy_order.quantity, 5)
+        self.assertEqual(matched_buy_order.status, OrderStatus.PARTIALLY_FILLED)
+        self.assertEqual(matched_sell_order.status, OrderStatus.FILLED)
 
     def test_multiple_matches(self):
-        buy_orders = {
-            100: [Order(1, OrderSide.BUY, 100, 10), Order(2, OrderSide.BUY, 100, 5)]
-        }
-        sell_orders = {
-            95: [Order(3, OrderSide.SELL, 95, 8)],
-            98: [Order(4, OrderSide.SELL, 98, 10)],
-        }
-        filled_orders = []
+        orders = [
+            Order(1, OrderSide.BUY, 100, 10),
+            Order(2, OrderSide.BUY, 100, 5),
+            Order(3, OrderSide.SELL, 95, 8),
+            Order(4, OrderSide.SELL, 98, 10)
+        ]
+        tracked_return = []
+        for order in orders:
+            self.order_queue.add_order(order)
+            self.order_queue.get_next_order()
+            num_removed, matches = self.match_engine.match_orders(self.order_queue)
+            tracked_return.append((num_removed, matches))
+        
+        # Check match orders after 3rd order is added
+        num_removed, matches = tracked_return[2]
+        matched_buy_order, matched_sell_order, *_ = matches[0]
+        self.assertEqual(matched_buy_order.order_id, "00000001")
+        self.assertEqual(matched_sell_order.order_id, "00000003")
 
-        num_removed, matches = self.match_engine.match_orders(
-            buy_orders, sell_orders, filled_orders
-        )
+        # Check match orders after 4th order is added
+        num_removed, matches = tracked_return[3]
+        matched_buy_order, matched_sell_order, *_ = matches[0]
+        self.assertEqual(matched_buy_order.order_id, "00000001")
+        self.assertEqual(matched_sell_order.order_id, "00000004")
+        matched_buy_order, matched_sell_order, *_ = matches[1]
+        self.assertEqual(matched_buy_order.order_id, "00000002")
+        self.assertEqual(matched_sell_order.order_id, "00000004")
 
-        self.assertEqual(num_removed, 3)
-        self.assertEqual(len(matches), 3)
-        self.assertEqual(len(filled_orders), 3)
-
-        if 100 in buy_orders:
-            self.assertEqual(buy_orders[100][0].quantity, 10)
-            self.assertEqual(buy_orders[100][0].status, OrderStatus.PROCESSING)
-        else:
-            self.assertTrue(
-                any(
-                    order.order_id == "00000001" and order.quantity == 0
-                    for order in filled_orders
-                )
-            )
-            self.assertTrue(
-                any(
-                    order.order_id == "00000002" and order.quantity == 0
-                    for order in filled_orders
-                )
-            )
+        self.assertEqual(len(self.order_queue.buy_orders), 0)
+        self.assertEqual(len(self.order_queue.sell_orders), 1)
+        self.assertEqual(self.order_queue.sell_orders[0].order.quantity, 3)
+        self.assertEqual(len(self.order_queue.filled_orders), 3)
 
     def test_no_match(self):
-        buy_orders = {90: [Order(1, OrderSide.BUY, 90, 10)]}
-        sell_orders = {100: [Order(2, OrderSide.SELL, 100, 10)]}
-        filled_orders = []
+        orders = [
+            Order(1, OrderSide.BUY, 90, 10),
+            Order(2, OrderSide.SELL, 100, 10),
+        ]
+        for order in orders:
+            self.order_queue.add_order(order)
+            self.order_queue.get_next_order()
 
-        num_removed, matches = self.match_engine.match_orders(
-            buy_orders, sell_orders, filled_orders
-        )
+        num_removed, matches = self.match_engine.match_orders(self.order_queue)
 
         self.assertEqual(num_removed, 0)
         self.assertEqual(len(matches), 0)
-        self.assertEqual(len(filled_orders), 0)
+        self.assertEqual(len(self.order_queue.filled_orders), 0)
 
 
 if __name__ == "__main__":
